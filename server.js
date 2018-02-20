ef681c579f779f6c3582d2d5bd71be32ffe65e1f
“innocent <rockcoolsaint@gmail.com>
2018-02-20 22:25:55 +0100

    serverjs added

---
+++ server.js
@@ -0,0 +1,30 @@
+/* eslint-disable no-param-reassign */
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+
+app.set('port', (process.env.PORT || 3000));
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Expires', '0');
+  next();
+});
+
+app.get('/molasses', (_, res) => {
+  setTimeout(() => {
+    res.end();
+  }, 5000);
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
+});
